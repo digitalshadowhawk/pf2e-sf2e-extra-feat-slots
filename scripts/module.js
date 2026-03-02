f001818d990873bbc805380c54cb1122194005bc
digitalshadowhawk <sonofhawk01@gmail.com>
2026-03-02 14:59:35 -0600

    Added Custom Feat section
    - added two new settings, Custom Feat Section and Custom Feat Section Header, allowing customization of a simple feat section that grants one feat at each level from 1-20.

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,7 @@
 Hooks.on('init', function() {
 	game.settings.register("pf2e-sf2e-extra-feat-slots", "ancestryParagon", {
 		name: "pf2e-sf2e-extra-feat-slots.SETTINGS.ancestryParagon.name",
-        hint: "pf2e-sf2e-extra-feat-slots.SETTINGS.ancestryParagon.hint",
+    hint: "pf2e-sf2e-extra-feat-slots.SETTINGS.ancestryParagon.hint",
 		scope: "world",
 		config: "true",
     requiresReload: true,
@@ -10,7 +10,7 @@ Hooks.on('init', function() {
 	});
 	game.settings.register("pf2e-sf2e-extra-feat-slots", "skillParagon", {
 		name: "pf2e-sf2e-extra-feat-slots.SETTINGS.skillParagon.name",
-        hint: "pf2e-sf2e-extra-feat-slots.SETTINGS.skillParagon.hint",
+    hint: "pf2e-sf2e-extra-feat-slots.SETTINGS.skillParagon.hint",
 		scope: "world",
 		config: "true",
     requiresReload: true,
@@ -28,7 +28,32 @@ Hooks.on('init', function() {
 		  type: Boolean
 	  });
   }
+  game.settings.register("pf2e-sf2e-extra-feat-slots", "custom", {
+		name: "pf2e-sf2e-extra-feat-slots.SETTINGS.custom.name",
+    hint: "pf2e-sf2e-extra-feat-slots.SETTINGS.custom.hint",
+		scope: "world",
+		config: "true",
+    requiresReload: true,
+		default: "false",
+		type: Boolean
+	});
+  game.settings.register("pf2e-sf2e-extra-feat-slots", "customName", {
+		name: "pf2e-sf2e-extra-feat-slots.SETTINGS.customName.name",
+    hint: "pf2e-sf2e-extra-feat-slots.SETTINGS.customName.hint",
+		scope: "world",
+		config: "true",
+    requiresReload: false,
+		default: "Custom Feat Section",
+		type: String,
+    onChange: (value) => {
+      const campaignFeatSections = game.settings.get(game.system.id, "campaignFeatSections")
+      campaignFeatSections[campaignFeatSections.findIndex((section) => section.id === "custom")].label = value;
+      game.settings.set(game.system.id, "campaignFeatSections", campaignFeatSections);
+    }
+	});
 });
+
+
 
 Hooks.once("ready", () => {
     variantFeats();
@@ -38,6 +63,7 @@ async function variantFeats() {
   const ancestryParagon = game.settings.get("pf2e-sf2e-extra-feat-slots", "ancestryParagon");
   const skillParagon = game.settings.get("pf2e-sf2e-extra-feat-slots", "skillParagon");
   const magaambyaBenefits = game.settings.get("pf2e-sf2e-extra-feat-slots", "magaambyaBenefits");
+  const custom = game.settings.get("pf2e-sf2e-extra-feat-slots", "custom");
 
   // Add campaign feat sections if enabled
   if (ancestryParagon || skillParagon || magaambyaBenefits) {  
@@ -71,20 +97,27 @@ async function variantFeats() {
         campaignFeatSections.push({
           id: "magaambyaBranches",
           label: "pf2e-sf2e-extra-feat-slots.SETTINGS.magaambyaBenefits.categories.branchLabel",
-          //supported: ["class"],
           slots: [1]
         });
         campaignFeatSections.push({
           id: "magaambyaBenefits",
           label: "pf2e-sf2e-extra-feat-slots.SETTINGS.magaambyaBenefits.categories.benefitsLabel",
-          //supported: ["skill"],
           slots: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         });
         campaignFeatSections.push({
           id: "magaambyaBenefitsSecondary",
           label: "pf2e-sf2e-extra-feat-slots.SETTINGS.magaambyaBenefits.categories.benefitsLabelSecondary",
-          //supported: ["skill"],
           slots: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+        });
+      }
+    }
+
+    if (custom) {
+      if (!campaignFeatSections.find((section) => section.id === "custom")) {
+        campaignFeatSections.push({
+          id: "custom",
+          label: game.settings.get("pf2e-sf2e-extra-feat-slots", "customName"),
+          slots: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         });
       }
     }
@@ -149,6 +182,18 @@ async function variantFeats() {
       1,
     );
     
+    await game.settings.set(game.system.id, "campaignFeatSections", campaignFeatSections);
+  }
+
+  if (
+    campaignFeatSections &&
+    !custom &&
+    campaignFeatSections.find((section) => section.id === "custom")
+  ) {
+    campaignFeatSections.splice(
+      campaignFeatSections.findIndex((section) => section.id === "custom"),
+      1,
+    );
     await game.settings.set(game.system.id, "campaignFeatSections", campaignFeatSections);
   }
 }
